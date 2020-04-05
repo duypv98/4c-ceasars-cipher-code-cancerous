fb2a78be9e811eb2a26267eef00b143c3329c3f9
Duy Phung <duypv98@gmail.com>
2020-04-05 14:06:26 +0700

    feat: click F*ckText and change to Normalize

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # styling-ceasars-cipher
-Encoding/Decoding with Ceasar's Cipher and Making Stylist Text.
 
+Encoding/Decoding with Ceasar's Cipher and Making Stylist Text.
 
 GitHub Page: [SCC](https://duypv98.github.io/styling-ceasars-cipher/)
--- js/app.js
+++ js/app.js
@@ -6,132 +6,99 @@ const eodChoice = document.getElementById('choice');
 const key = document.getElementById('key');
 const modal = document.getElementById('notice-modal');
 const closeBtn = document.getElementById('close-btn');
+const modeBtn = document.getElementById('mode-btn');
 
-function updateShiftKey(val) {
-    theKey.value = val;
+const FORMAT_MODE = {
+    cancerous: 'c',
+    normal: 'n'
 }
+
+let formatModeValue = FORMAT_MODE.cancerous;
+
+function updateShiftKey(val) { theKey.value = val; }
 function getCode() {
-    if (inputText.value == null || !inputText.value) {
+    if (inputText.value === null || inputText.value === "" || !inputText.value) {
         modal.style.display = "block";
         return;
     }
-    let eod = eodChoice.value;
-    if (eod == 'encode') {
-        encode();
+    const eod = eodChoice.value;
+    const shiftKey = eod === 'encode' ? parseInt(theKey.value) : parseInt(theKey.value) * - 1;
+    const txt = inputText.value;
+    const x = [];
+
+    for (let i = 0; i < txt.length; i++) {
+        x[i] = txt.charCodeAt(i) + shiftKey;
+        if (txt.charCodeAt(i) >= 65 && txt.charCodeAt(i) <= 90) {
+            if (x[i] >= 65 && x[i] <= 90) {
+                x[i] = x[i];
+            }
+            else if (x[i] > 90) {
+                x[i] -= 26;
+            }
+            else if (x[i] < 65) {
+                x[i] += 26;
+            }
+        }
+        if (txt.charCodeAt(i) === 32 || txt.charCodeAt(i) < 65 || txt.charCodeAt(i) > 122
+            || (txt.charCodeAt(i) >= 91 && txt.charCodeAt(i) <= 96)) {
+            x[i] = txt.charCodeAt(i);
+        }
+        if (txt.charCodeAt(i) >= 97 && txt.charCodeAt(i) <= 122) {
+            if (x[i] >= 97 && x[i] <= 122) {
+                x[i] = x[i];
+            }
+            else if (x[i] > 122) {
+                x[i] -= 26;
+            }
+            else if (x[i] < 97) {
+                x[i] += 26;
+            }
+        }
     }
-    else {
-        decode();
-    }
-    format.setAttribute('style', 'color: green;');
+    resultText.value = String.fromCharCode.apply(null, x);
+    if (formatModeValue === FORMAT_MODE.normal) {
+        format.innerHTML = 'Normal';
+        format.setAttribute('style', 'color: green;');
+        modeBtn.setAttribute('class', 'btn btn-warning btn-lg');
+        modeBtn.innerHTML = 'F*ck Text';
+        formatModeValue = FORMAT_MODE.cancerous;
+    };
+    modeBtn.disabled = false;
 }
 
 closeBtn.onclick = () => {
     modal.style.display = "none";
 }
 window.onclick = (event) => {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+    if (event.target === modal) modal.style.display = "none";
 }
 window.onkeydown = (event) => {
-    if (event.keyCode == 27) {
-        modal.style.display = "none";
-    }
-}
-
-function encode() {
-    const shiftKey = parseInt(theKey.value);
-    const txt = inputText.value;
-    let x = [];
-
-    for (let i = 0; i < txt.length; i++) {
-        x[i] = txt.charCodeAt(i) + shiftKey;
-        if (txt.charCodeAt(i) >= 65 && txt.charCodeAt(i) <= 90) {
-            if (x[i] >= 65 && x[i] <= 90) {
-                x[i] = x[i];
-            }
-            else if (x[i] > 90) {
-                x[i] -= 26;
-            }
-            else if (x[i] < 65) {
-                x[i] += 26;
-            }
-        }
-        if (txt.charCodeAt(i) == 32 || txt.charCodeAt(i) < 65 || txt.charCodeAt(i) > 122
-            || (txt.charCodeAt(i) >= 91 && txt.charCodeAt(i) <= 96)) {
-            x[i] = txt.charCodeAt(i);
-        }
-        if (txt.charCodeAt(i) >= 97 && txt.charCodeAt(i) <= 122) {
-            if (x[i] >= 97 && x[i] <= 122) {
-                x[i] = x[i];
-            }
-            else if (x[i] > 122) {
-                x[i] -= 26;
-            }
-            else if (x[i] < 97) {
-                x[i] += 26;
-            }
-        }
-    }
-    let _res = String.fromCharCode.apply(null, x);
-    resultText.value = _res;
-    format.innerHTML = 'Normal';
-}
-
-function decode() {
-    let shiftKey = parseInt(theKey.value);
-    shiftKey *= -1;
-    let txt = inputText.value;
-
-    let x = [];
-
-    for (let i = 0; i < txt.length; i++) {
-        x[i] = txt.charCodeAt(i) + shiftKey;
-        if (txt.charCodeAt(i) >= 65 && txt.charCodeAt(i) <= 90) {
-            if (x[i] >= 65 && x[i] <= 90) {
-                x[i] = x[i];
-            }
-            else if (x[i] > 90) {
-                x[i] -= 26;
-            }
-            else if (x[i] < 65) {
-                x[i] += 26;
-            }
-        }
-        if (txt.charCodeAt(i) == 32 || txt.charCodeAt(i) < 65 || txt.charCodeAt(i) > 122
-            || (txt.charCodeAt(i) >= 91 && txt.charCodeAt(i) <= 96)) {
-            x[i] = txt.charCodeAt(i);
-        }
-        if (txt.charCodeAt(i) >= 97 && txt.charCodeAt(i) <= 122) {
-            if (x[i] >= 97 && x[i] <= 122) {
-                x[i] = x[i];
-            }
-            else if (x[i] > 122) {
-                x[i] -= 26;
-            }
-            else if (x[i] < 97) {
-                x[i] += 26;
-            }
-        }
-    }
-    let _res = String.fromCharCode.apply(null, x);
-    resultText.value = _res;
-    format.innerHTML = 'Normal';
+    if (event.keyCode === 27) modal.style.display = "none"; // Hit ESC to close the modal
 }
 
 function cancerous() {
-    if (inputText.value == null || !inputText.value) {
+    if (inputText.value === null || inputText.value === "" || !inputText.value) {
         modal.style.display = "block";
         return;
     }
-    let tmp = resultText.value;
-    resultText.value = change(tmp);
-    format.innerHTML = 'Cancerous';
-    format.setAttribute('style', 'color: red;');
+
+    if (formatModeValue === FORMAT_MODE.cancerous) {
+        resultText.value = change(resultText.value);
+        format.innerHTML = 'Cancerous';
+        format.setAttribute('style', 'color: red;');
+        modeBtn.innerHTML = 'Normalize';
+        modeBtn.setAttribute('class', 'btn btn-success btn-lg');
+        formatModeValue = FORMAT_MODE.normal;
+    } else if (formatModeValue === FORMAT_MODE.normal) {
+        getCode();
+        modeBtn.innerHTML = 'F*ck Text';
+        modeBtn.setAttribute('class', 'btn btn-warning btn-lg');
+        formatModeValue = FORMAT_MODE.cancerous;
+    }
 }
 
 function change(str) {
-    let cancerousStr = str.toLowerCase().split('');
+    const cancerousStr = str.toLowerCase().split('');
     for (let i = 0; i < cancerousStr.length; i++) {
         if (i % 2 != 0) {
             cancerousStr[i] = cancerousStr[i].toUpperCase();
@@ -145,4 +112,7 @@ function resetValue() {
     resultText.value = "";
     theKey.value = 0;
     key.value = 0;
+    modeBtn.value = 'C';
+    modeBtn.innerHTML = 'F*ck Text';
+    modeBtn.disabled = true;
 }
